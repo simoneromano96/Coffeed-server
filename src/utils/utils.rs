487074e5e0a978a923ac3f3fb58bde6b2b99927f
Simone Romano <simoneromano@protonmail.ch>
2019-10-27 17:30:10 +0100

    Updated cargo

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -9,7 +9,9 @@ pub struct PasswordHasher {
 
 impl PasswordHasher {
     /// Build hasher and verifier wrapper
-    pub fn build(secret_key: String) -> PasswordHasher {
+    pub fn build() -> PasswordHasher {
+        // Get the secret key
+        let secret_key = std::env::var("HASH_SECRET_KEY").unwrap();
         // Hasher
         let mut hasher = Hasher::default();
         hasher.with_secret_key(secret_key.clone());
@@ -26,6 +28,7 @@ impl PasswordHasher {
             Err(error) => Err(error.to_string()),
         }
     }
+    /// Verify hashed password
     pub fn verify(&mut self, hash: String, password: String) -> Result<bool, String> {
         match self
             .verifier
@@ -37,4 +40,26 @@ impl PasswordHasher {
             Err(error) => Err(error.to_string()),
         }
     }
+}
+
+// JWT
+#[derive(Serialize, Deserialize)]
+struct Claims {
+    pub iss: String,
+    pub sub: String,
+    pub exp: i32,
+}
+
+pub fn create_token(username: String) -> String {
+    let jwt_secret = std::env::var("JWT_SECRET_KEY").unwrap();
+    let jwt_issuer = std::env::var("JWT_ISSUER").unwrap();
+    let jwt_expiry = std::env::var("JWT_EXPIRY").unwrap().parse::<i32>().unwrap();
+    let headers = Header::default();
+    let claims = Claims {
+        iss: jwt_issuer,
+        sub: username,
+        exp: jwt_expiry,
+    };
+
+    encode(&headers, &claims, jwt_secret.clone().as_bytes()).unwrap()
 }
