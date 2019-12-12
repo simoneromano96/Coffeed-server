e9ceb88a804bad01b889e79ef06d79ffaf057b36
Simone Romano <simoneromano@protonmail.ch>
2019-12-12 20:03:54 +0100

    Update Cargo.toml and routes.rs Revert to actix 1.x

--- api-gateway/src/auth_service/routes.rs
+++ api-gateway/src/auth_service/routes.rs
@@ -1,15 +1,11 @@
 // Crates
 use crate::AppState;
 use actix_session::Session;
+use actix_web::client as awc;
 use actix_web::{
     cookie::CookieJar, error, web, Error, HttpMessage, HttpRequest, HttpResponse, Responder,
 };
-use futures::Future;
-use reqwest::{
-    self,
-    header::{HeaderMap, HeaderValue, FORWARDED},
-    Url,
-};
+use futures::{Future, Stream};
 use serde_derive::{Deserialize, Serialize};
 use std::env;
 
@@ -41,6 +37,46 @@ pub struct LoginInfo {
     password: String,
 }
 
+fn forward_to(
+    destination_address: String,
+    client: awc::Client,
+    body: web::Bytes,
+    req: HttpRequest,
+) -> impl Future<Item = HttpResponse, Error = Error> {
+    // Create a new request
+    let forwarded_req = client
+        .request_from(destination_address, req.head())
+        .no_decompress();
+    // Add headers
+    let forwarded_req = if let Some(addr) = req.head().peer_addr {
+        forwarded_req
+            .header("x-forwarded-for", format!("{}", addr.ip()))
+            .header("forwarded", format!("for={}", addr.ip()))
+    } else {
+        forwarded_req
+    };
+
+    forwarded_req
+        .send_body(body)
+        .map_err(Error::from)
+        .map(|mut res| {
+            let mut client_resp = HttpResponse::build(res.status());
+            // Remove `Connection` as per
+            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
+            for (header_name, header_value) in
+                res.headers().iter().filter(|(h, _)| *h != "connection")
+            {
+                client_resp.header(header_name.clone(), header_value.clone());
+            }
+            res.body()
+                .into_stream()
+                .concat2()
+                .map(move |b| client_resp.body(b))
+                .map_err(|e| e.into())
+        })
+        .flatten()
+}
+
 pub fn login(
     app_state: web::Data<AppState>,
     // login_info: web::Json<LoginInfo>,
@@ -56,112 +92,24 @@ pub fn login(
         &API_ROUTE.parse::<String>().unwrap(),
         &LOGIN_ROUTE.parse::<String>().unwrap(),
     );
-    // Then Parse it into URL
-    // let destination_address: Url = destination_address_string.parse::<Url>().unwrap();
-
-    // Create a new request
-    let forwarded_req = client
-        .request_from(destination_address, req.head())
-        .no_decompress();
-    // Add headers
-    let forwarded_req = if let Some(addr) = req.head().peer_addr {
-        forwarded_req
-            .header("x-forwarded-for", format!("{}", addr.ip()))
-            .header("forwarded", format!("for={}", addr.ip()))
-    } else {
-        forwarded_req
-    };
-
-    forwarded_req
-        .send()
-        .map(|mut response| {
-            let mut http_response = HttpResponse::build(response.status());
-            // Remove `Connection` as per
-            // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
-            for (header_name, header_value) in response
-                .headers()
-                .iter()
-                .filter(|(h, _)| *h != "connection")
-            {
-                http_response.header(header_name.clone(), header_value.clone());
-            }
-            response
-                .body()
-                .map(|body| Ok(body))
-                .map_err(|error| Err(error))
-        })
-        .flatten()
-        .map_err(Error::from)
-        .map(|a| {})
+    forward_to(destination_address, client, body, req)
 }
 
 pub fn logout(
     app_state: web::Data<AppState>,
+    body: web::Bytes,
     req: HttpRequest,
 ) -> impl Future<Item = HttpResponse, Error = Error> {
     // Get client
     let client = app_state.http_client.clone();
     // Create url string
-    let destination_address_string: String = format!(
+    let destination_address: String = format!(
         "{}{}{}",
         &AUTH_SERVICE_URL.parse::<String>().unwrap(),
         &API_ROUTE.parse::<String>().unwrap(),
         &LOGOUT_ROUTE.parse::<String>().unwrap(),
     );
-    // Then Parse it into URL
-    let destination_address: Url = destination_address_string.parse::<Url>().unwrap();
-
-    // Get request ip
-    let from_address = req.head().peer_addr.unwrap();
-
-    // Get request cookie
-    // let actix_session_cookie = req.cookie("actix-session").unwrap();
-
-    // Create headers
-    let mut header_map: HeaderMap = HeaderMap::new();
-
-    // Set forwarded header
-    header_map.append(
-        FORWARDED,
-        HeaderValue::from_str(&from_address.to_string()).unwrap(),
-    );
-    /*
-    header_map.append(
-        "actix-session",
-        HeaderValue::from_str(actix_session_cookie.name()).unwrap(),
-    );
-    */
-
-    web::block(move || {
-        let result: Result<reqwest::Response, reqwest::Error> =
-            client.post(destination_address).headers(header_map).send();
-
-        match result {
-            Ok(mut response) => {
-                let mut cookie_jar = CookieJar::new();
-                let cookies = response.cookies();
-                cookies.for_each(|cookie| {
-                    let actix_cookie = actix_web::cookie::Cookie::new(
-                        cookie.name().to_owned(),
-                        cookie.value().to_owned(),
-                    );
-                    actix_cookie.set_expires(cookie.expires().unwrap());
-                    cookie_jar.add(actix_cookie);
-                });
-
-                Ok((response.text().unwrap(), cookie_jar))
-            }
-            Err(e) => Err(e.to_string()),
-        }
-    })
-    .map(|(data, cookies)| {
-        let mut response_builder = HttpResponse::Ok();
-        cookies.iter().for_each(|cookie| {
-            response_builder.cookie(cookie.to_owned());
-        });
-        response_builder.json(data)
-    })
-    .map_err(error::ErrorInternalServerError)
+    forward_to(destination_address, client, body, req)
 }
 
 pub fn get_session(session: Session) -> impl Responder {
